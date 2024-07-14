b97efd310d73969d15b6bb0d1d567fcce0b58d7a
Ege Aksoz <egeaksoz@gmail.com>
2024-07-14 19:06:51 +0100

    finished placeholder text animation

--- src/sharedComponents/Input/InputBox.tsx
+++ src/sharedComponents/Input/InputBox.tsx
@@ -9,7 +9,8 @@ const InputBox: FC<InputTextProps> = ( {type}: InputTextProps ) => {
   const placeholderText = `${type?.charAt(0).toUpperCase()}${type?.slice(1)}`
   return (
     <div className="creds">
-      <input id={type} name={type} placeholder={placeholderText} aria-label={type} autoComplete={type} type={type == "password" ? "password" : "text"}/>
+      <input id={type} name={type} aria-label={type} autoComplete={type} type={type == "password" ? "password" : "text"} required/>
+      <label htmlFor={type} aria-hidden="true">{placeholderText}</label>
     </div>
   )
 };
