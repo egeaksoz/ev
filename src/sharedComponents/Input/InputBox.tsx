cf671197d7dac30b579a567c6135f3da7933e318
Ege Aksoz <egeaksoz@gmail.com>
2024-07-23 06:07:56 +0100

    add visibility icon to password input

--- src/sharedComponents/Input/InputBox.tsx
+++ src/sharedComponents/Input/InputBox.tsx
@@ -1,5 +1,6 @@
 import { FC, useState } from "react";
 import './InputBox.scss'
+import { Visibility, VisibilityOff } from  '@mui/icons-material'
 
 interface InputTextProps {
   type?: string
@@ -8,20 +9,18 @@ interface InputTextProps {
 const InputBox: FC<InputTextProps> = ( {type}: InputTextProps ) => {
   const placeholderText = `${type?.charAt(0).toUpperCase()}${type?.slice(1)}`
   const [inputType, setInputType] = useState(type)  
-  const [showText, setShowText] = useState("Show")
+  const [toggleVisibilityIcon, setToggleVisibilityIcon] = useState(true)
 
   const handleToggle = () => {
-    const newType = inputType === "password" ? "text" : "password"
-    setInputType(newType)
-    const newShowText = showText === "Show" ? "Hide" : "Show"
-    setShowText(newShowText)
+    setInputType(inputType === "password" ? "text" : "password")
+    setToggleVisibilityIcon(!toggleVisibilityIcon)
   }
 
   return (
     <div className="creds">
       <input id={type} name={type} aria-label={type} autoComplete={type} type={inputType} required/>
       <label htmlFor={type} aria-hidden="true">{placeholderText}</label>
-      {type === "password" && <span id="show-password" role="button" onClick={handleToggle}>{showText}</span> }
+      {type === "password" && <span role="button" onClick={handleToggle}>{toggleVisibilityIcon ? <Visibility /> : <VisibilityOff />}</span> }      
     </div>
   )
 };
