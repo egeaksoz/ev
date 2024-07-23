5953f2ad9559cf875abfa0f2358ccb46ab55cb14
Ege Aksoz <egeaksoz@gmail.com>
2024-07-23 06:23:26 +0100

    fix responsive span issue

--- src/sharedComponents/Footer/Footer.tsx
+++ src/sharedComponents/Footer/Footer.tsx
@@ -6,7 +6,7 @@ const currentYear = new Date().getFullYear()
 const Footer: FC = () => {
   return (
     <footer>
-        <p> © {currentYear} Ege Aksoz <span> - Our house in the middle of our street </span></p>
+        <p> © {currentYear} Ege Aksoz <span className='motto'> - Our house in the middle of our street </span></p>
         <a href='https://github.com/egeaksoz/ev'><img src='github-mark.png' /></a>
     </footer>
   )
